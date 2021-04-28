1c67e580480191014c6f7aa179c0f5c1667768db
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-04-28 09:27:46 +0100

    optimization #1

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
 const cheerio = require('cheerio')
 const puppeteer = require('puppeteer-extra');
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
 
-
-const getLinkPreviewData = async (link, puppeteerArgs = []) => {
+const puppeteerArgs = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain'
+]
+const getLinkPreviewData = async (link) => {
     //launching the browser with the headless mode
     const browser = await puppeteer.launch({
         headless: true,
         args: [
             ...puppeteerArgs,
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
+
         ],
     });
     puppeteer.use(pluginStealth())
+    puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+
 
     try {
         /** got to the page and wait until render and load all scripts also non-crucial element */
-        const page = await browser.newPage();
+        const page = (await browser.pages())[0]
+        await page.setRequestInterception(true);
+
+        page.on('request', request => {
+            if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet')
+                request.abort();
+            else
+                request.continue();
+        });
         await page.goto(link, { waitUntil: 'networkidle0' });
 
         /**getting data from page content */
